a77786250b90ec6099b5ab2f6d1a67680310f7b9
kds-student <zvonimir.duhovic@gmail.com>
2024-09-25 08:37:20 +0200

    add variable for default date

--- models/staging/tech_store/stg_tech_store__employees.sql
+++ models/staging/tech_store/stg_tech_store__employees.sql
@@ -14,7 +14,7 @@ final as (
         lname as last_name,
         concat(first_name, ' ', last_name) as full_name,
         hiredate as hired_at,
-        enddate as terminated_at,
+        ifNull(toString(enddate), '{{ var("default_date") }}') as terminated_at,
         if(terminated_at is null, true, false) as is_active 
     from employees
 
